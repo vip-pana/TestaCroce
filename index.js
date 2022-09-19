87dc96732f64d2d12bf8f31f607439a33811bb22
P4na <Vip_P4na@protonmail.com>
2022-09-19 16:11:20 +0200

    sistemato interfaccia, potenzialmente migliorabile ma gia' decente, manca solo un sistema di alert per i tasti non premuti

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 let difficulty = 0.50;
 let resultHtml = document.getElementById("resultHtml");
+let playerScoreHtml = document.getElementById("playerScore");
+let playerScore = 0;
+let computerScoreHtml = document.getElementById("computerScore");
+let computerScore = 0;
+let playerWin = null;
+let startGameButton = document.getElementById("startGame");
 
+function updateScore(playerWin){
+    if(playerWin == true){
+        playerScore++;
+        playerScoreHtml.innerHTML = playerScore;
+    }else{
+        computerScore++;
+        computerScoreHtml.innerHTML = computerScore;
+    }
+}
+
+function setDifficulty(){
+    console.log(difficulty)
+    difficulty = document.querySelector("input[name = 'difficulty']:checked").value;}
+
+function endScore(){
+    if(playerScore > 4 || computerScore > 4){
+        resultHtml.innerHTML = "Gioco finito";
+        startGameButton.disabled = true;
+    }
+}
 
 function startGame(){
+    /* controllo se il risultato ha superato il limite */
+    endScore();
+    setDifficulty();
+    /* inizia il gioco, seleziono la faccia, in lancio metto un valore percentuale  */
     let choice = document.querySelector("input[name = 'choice']:checked").value; 
-    console.log(choice);
+    console.log(choice)
     let lancio = Math.random();
+    console.log(lancio);
     if(lancio <= difficulty){
         lancio = "testa";
     } else {
@@ -13,7 +44,10 @@ function startGame(){
     }
     if (lancio == choice){
         resultHtml.innerHTML = "hai vinto";
+        updateScore(playerWin=true);
     } else {
         resultHtml.innerHTML = "hai perso";
+        updateScore(playerWin=false)
     }
+    endScore();
 }
