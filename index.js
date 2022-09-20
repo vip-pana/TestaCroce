b2518fc27c7c1e6b93afd2a6ee75794ffe09aa0e
P4na <Vip_P4na@protonmail.com>
2022-09-20 16:39:08 +0200

    finito il loading, e inserito il riprova

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-let resultHtml = document.getElementById("resultHtml");
-// punteggi per giocatori
+let resultHtml = document.getElementById("resultHtml"); //stampa se hai vinto o perso
+let gifHtml = document.getElementById("gifHtml");
+let cardsHtml = document.getElementById("cards");
+// punteggi per giocatori e html
 let playerScoreHtml = document.getElementById("playerScore");
 let playerScore = 0;
 let computerScoreHtml = document.getElementById("computerScore");
 let computerScore = 0;
-let playerWin = null;
-let startGameButton = document.getElementById("startGame");
 
-// punteggio per countoneMilion
+let startGameButton = document.getElementById("startGame"); // bottone di lancio moneta
+
+// punteggio per countOneMilion
 let croceScoreHtml = document.getElementById("croceScore");
 let testaScoreHtml = document.getElementById("testaScore");
 let testaScore = 0;
-let crociaScore = 0;
-
+let croceScore = 0;
+let retryHtml = document.getElementById("retry");
 
 function updateScore(playerWin){
     if(playerWin == true){
@@ -25,26 +27,36 @@ function updateScore(playerWin){
 }
 
 function setDifficulty(){
-    difficulty = document.querySelector("input[name = 'difficulty']:checked").value;}
+    difficulty = document.querySelector("input[name = 'difficulty']:checked").value;
+    return difficulty;
+}
+
+function end(){
+    resultHtml.innerHTML = "<center>Gioco finito</center>";
+    startGameButton.disabled = true;
+    retryHtml.hidden = false
+}
 
 function endScore(){
     if(playerScore > 4 || computerScore > 4){
-        resultHtml.innerHTML = "Gioco finito";
-        startGameButton.disabled = true;
+        end()
     }
 }
 
-function startGame(){
-    /* controllo se il risultato ha superato il limite */
-    endScore();
-    setDifficulty();
-    /* inizia il gioco, seleziono la faccia, in lancio metto un valore percentuale  */
+function compare(difficulty, lancio){
     let choice = document.querySelector("input[name = 'choice']:checked").value; 
-    let lancio = Math.random();
-    if(lancio <= difficulty){
-        lancio = "testa";
+    if(choice == "testa"){
+        if(lancio >= difficulty){
+            lancio = "testa";
+        } else {
+            lancio = "croce";
+        }
     } else {
-        lancio = "croce";
+        if(lancio <= difficulty){
+            lancio = "testa";
+        } else {
+            lancio = "croce";
+        }
     }
     if (lancio == choice){
         resultHtml.innerHTML = "hai vinto";
@@ -53,23 +65,52 @@ function startGame(){
         resultHtml.innerHTML = "hai perso";
         updateScore(playerWin=false)
     }
+}
+
+function loading() {
+    cardsHtml.hidden = false;
+    gifHtml.hidden = true;
+    let difficulty = setDifficulty();
+    let lancio = Math.random();
+    compare(difficulty, lancio);
+}
+
+function startGame(){
+    /* controllo se il risultato ha superato il limite  e inserisco la difficolta */
+    endScore();
+    setTimeout(loading, 3500)
+    /* inizia il gioco, seleziono la faccia, in lancio metto un valore percentuale  */
+    cardsHtml.hidden = true;
+    gifHtml.hidden = false;
     endScore();
 }
 
 function countOneMilion(){
+    
+    setTimeout(counting, 1000)
+    
+    cardsHtml.hidden = true;
+    gifHtml.hidden = false;
+    
+    }
+
+
+function counting(){
+    cardsHtml.hidden = false;
+    gifHtml.hidden = true;
     for(let i = 0; i < 100000; i++){
         let difficulty = 0.50;
         let lancio = Math.random();
         if(lancio <= difficulty){
             lancio = "testa";
             testaScore++;
-            testaScoreHtml.innerHTML = testaScore;
         } else {
             lancio = "croce";
-            crociaScore++;
-            
+            croceScore++;
         }
         testaScoreHtml.innerHTML = testaScore;
-        croceScoreHtml.innerHTML = crociaScore;
-    }
+        croceScoreHtml.innerHTML = croceScore;
+        end();
+        
+}
 }
